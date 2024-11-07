f9379f46a9cc293b653d4445a95929e9c53eb6f8
BlasDeLezoEsGod <arnaucebrianortega@gmail.com>
2024-11-07 20:00:06 +0100

    next_line

--- get_next_line.c
+++ get_next_line.c
@@ -1,89 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: arcebria <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 19:47:39 by arcebria          #+#    #+#             */
-/*   Updated: 2024/10/30 20:39:19 by arcebria         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
-#include <stdio.h>
-#include <unistd.h>
 
-char	*get_next_line(int fd)
+char    *read_line(char *static_line)
 {
-	static char	*line;
-	int		bytes_read;
-	int		i;
-	char	*buff;
+    char    *buff;
+    int bytes_read;
 
-	buff = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-	if (!buff)
-		return (NULL);
-	if (!line)
-		line = ft_calloc(1,1);
-	//bytes_read = read(fd, buff, BUFFER_SIZE);
-	bytes_read = 1;
-	i = 0;
-	while (bytes_read > 0 && (!ft_strchr(line, '\n')))
-	{
-		bytes_read = read(fd, buff, BUFFER_SIZE);
-		buff[bytes_read] = '\0';
-		line = ft_strjoin(line, buff);
-		//printf("esto es buffer %s\n", buff);
-	}
-	//printf("esto es buffer %s", buff);
-	return (line);
 
-	/*while (buff[i])
-	{
-		if (buff[i] == '\n')
-		{
-			buff[bytes_read] = '\0';
-			//printf("Soy el buffer: |%s|\n", buff);
-			return (buff);
-		}
-		i++;
-	}
-	line = ft_strjoin(line, buff);
-	line[bytes_read] = '\0';
-	//printf ("Soy la line: |%s|\n", line);
-	return (line);*/
 }
-int main(void)
+
+char    *get_next_line (int fd)
 {
-	char	*line;
-	int fd = open("text.txt", O_RDONLY);
-	if (fd == -1)
-		return (-1);
-//	printf("line = %s", get_next_line(fd));
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s", line);
-		free(line);
-	}
-	free(line);
-	close(fd);
-	return (0);
-}
-/*
-int	main()
-{	
-	int	fd;
-	char	*next_line;
+    static char *static_line;
+    char    line_def;
 
-	fd = open("text.txt", O_RDONLY);
-	if (!fd)
-	{
-		printf("Error en el archivo");
-		return (1);
-	}
-	while (read(fd, next_line, ))
-		printf("%s", get_next_line(fd));
-	close (fd);
-	return 0;
-}*/
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (0);
+    while(!static_line || !ft_strchr(static_line, '\n'))
+        read_line(static_line);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,30 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: arcebria <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 19:51:26 by arcebria          #+#    #+#             */
-/*   Updated: 2024/10/30 20:34:25 by arcebria         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE.H
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
-#endif
-
-#include <fcntl.h>
-#include <stddef.h>
-#include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
 
 char    *get_next_line(int fd);
+size_t  ft_strlen(char *s);
+char    *ft_strchr(char *s, int c);
 char    *ft_strjoin(char *s1, char *s2);
-void    *ft_calloc(size_t nmemb, size_t size);
-size_t  ft_strlen(const char *str);
-void    *ft_memcpy(void *dest, const void *src, size_t n);
-char	*ft_strchr(char *s, int c);
+char    *ft_calloc(int x, int y);
+
 #endif
